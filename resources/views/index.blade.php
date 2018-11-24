b6a21c897c68008d593c2862baeb39d89179f89f
Guillermo Tabligan <gtplus.net@gmail.com>
2018-11-24 15:19:24 +0800

    EZ DONE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,115 @@
 @extends('layout')
 @section('content')
 <div class="container">
-	<div class="row">
-		
+
+	<div id="logged" class="hidden">
+asdasdasd
+	</div>
+	<div id="logged" class="hidden">
+not logged
+	</div>
+	<div class="row" id="not_logged">
+		<!-- <div class="col-xs-12 col-md-12 m-3">
+			<small class="d-block font-weight-bold">
+				Name
+			</small>
+			<input type="text" id="wallet_name" class="form-control form-control-lg"> 
+		</div>
+		<div class="col-xs-12 col-md-12 m-3">
+			<small class="d-block font-weight-bold">
+				Email
+			</small>
+		<input type="text" id="wallet_email" class="form-control form-control-lg">
+		</div> -->
+		<div class="col-xs-12 col-md-12 m-3">
+			<small class="d-block font-weight-bold">
+				Current Address
+			</small>
+		<input type="text" id="wallet_address" class="form-control form-control-lg" disabled>
+		</div>
+		<!-- <div class="col-xs-12 col-md-12 m-3">
+			<button class="btn btn-lg btn-block btn-primary" onclick="onSubmit()">Sign Up</button>
+		</div> -->
+		<div class="col-xs-12 col-md-12 m-3">
+			<small class="d-block font-weight-bold">
+				To Address
+			</small>
+		<input type="text" id="wallet_address_2" class="form-control form-control-lg">
+		</div>
+		<div class="col-xs-12 col-md-12 m-3">
+			<small class="d-block font-weight-bold">
+				ETH Value to Send
+			</small>
+		<input type="text" id="eth_amount" class="form-control form-control-lg">
+		</div>
+		<div class="col-xs-12 col-md-12 m-3">
+			<small class="d-block font-weight-bold">
+				Current Balance
+			</small>
+		<input type="text" id="current_balance" class="form-control form-control-lg" disabled>
+		</div>
+		<div class="col-xs-12 col-md-12 m-3">
+			<button class="btn btn-lg btn-block btn-primary" onclick="onSend()">Send</button>
+		</div>
 	</div>
 </div>
+@endsection
+@section('js')
+<script>
+	$(document).ready(function(data){
+		if(typeof(localStorage.getItem('address')) != "undefined" && localStorage.getItem('address') !== null)
+		{
+			$("#not_logged").addClass("hidden")
+			$("#logged").removeClass("hidden")
+		}
+		else
+		{
+			console.log(456)
+			$("#not_logged").removeClass("hidden")
+			$("#logged").addClass("hidden")
+		}
+	})
+	
+
+
+	function onSubmit()
+	{
+		var name 	= $('#wallet_name').val()
+		var email 	= $('#wallet_email').val()
+		var address = $('#wallet_address').val()
+		
+		localStorage.setItem('name', name)
+		localStorage.setItem('email', email)
+		localStorage.setItem('address', address)
+		
+		
+		if(typeof(localStorage.getItem('address')) != "undefined" && localStorage.getItem('address') !== null)
+		{
+			$("#not_logged").addClass("hidden")
+			$("#logged").removeClass("hidden")
+			window.location.href = "/"
+		}
+		else
+		{
+			console.log(456)
+			$("#not_logged").removeClass("hidden")
+			$("#logged").addClass("hidden")
+		}
+	}
+	
+	function onSend()
+	{
+		var address_from = $('#wallet_address').val()
+		var address_to   = $('#wallet_address_2').val()
+		var eth          = $('#eth_amount').val()
+		
+		web3.eth.sendTransaction({
+			from: address_from,
+			to: address_to, 
+			value: web3.utils.toWei(eth, "ether")
+		})
+
+	}
+
+</script>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>DISH 2018</title>
+    <style>
+    .hidden{
+      display: none;
+    }
+    </style>
   </head>
   <body>
   	<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
@@ -29,5 +34,50 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/ethereum/web3.js@1.0.0-beta.36/dist/web3.min.js" integrity="sha256-nWBTbvxhJgjslRyuAKJHK+XcZPlCnmIAAMixz6EefVk=" crossorigin="anonymous"></script>
+    <script>
+      window.addEventListener('load', async () => {
+          // Modern dapp browsers...
+          if (window.ethereum) {
+              window.web3 = new Web3(ethereum);
+              try {
+                  // Request account access if needed
+                  await ethereum.enable();
+                  // Acccounts now exposed
+                  on_load(web3);
+              } catch (error) {
+                  // User denied account access...
+              }
+          }
+          // Legacy dapp browsers...
+          else if (window.web3) {
+              window.web3 = new Web3(web3.currentProvider);
+              // Acccounts always exposed
+              on_load(web3);
+          }
+          // Non-dapp browsers...
+          else 
+          {
+              console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+          }
+      });
+
+      function on_load(web3)
+      {
+        var wallet = web3.eth.accounts.wallet
+        var address = document.getElementById('wallet_address')
+        var current = document.getElementById('current_balance')
+        address.value = wallet._accounts.givenProvider.selectedAddress
+
+        web3.eth.getBalance(address.value, (err, balance) => 
+        {
+          balance = balance;
+          var current_balance = web3.utils.fromWei(balance, 'ether');
+
+          current.value = current_balance;
+        });
+      }
+    </script>
+    @yield('js')
   </body>
 </html>
